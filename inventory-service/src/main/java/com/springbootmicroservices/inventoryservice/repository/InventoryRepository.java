02eba050431c7dc3ff1a779b0ef9a4e1479be8ac
Chanuth10 <it20074418@my.sliit.lk>
2023-08-12 14:58:38 +0530

    add inventory service database

--- inventory-service/src/main/java/com/springbootmicroservices/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/springbootmicroservices/inventoryservice/repository/InventoryRepository.java
@@ -1,12 +1,14 @@
 package com.springbootmicroservices.inventoryservice.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springbootmicroservices.inventoryservice.model.Inventory;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    List<Inventory> findBySkuCodeIn(List<String> skuCode);
-    
+
+    Optional<Inventory> findBySkuCode();
+
 }
