c1a31f0f59f6d52378f0b4c9d6ed84c86725532f
Chanuth10 <it20074418@my.sliit.lk>
2023-08-12 23:35:00 +0530

    fix error

--- inventory-service/src/main/java/com/springbootmicroservices/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/springbootmicroservices/inventoryservice/controller/InventoryController.java
@@ -1,35 +1,20 @@
 package com.springbootmicroservices.inventoryservice.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import lombok.extern.slf4j.Slf4j;
-
-import com.springbootmicroservices.inventoryservice.dto.InventoryResponse;
 import com.springbootmicroservices.inventoryservice.service.InventoryService;
-
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
-@Slf4j
 public class InventoryController {
 
     private final InventoryService inventoryService;
 
-    // http://localhost:8082/api/inventory/iphone-13,iphone13-red
-
-    // http://localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone13-red
-    @GetMapping
+    @GetMapping("/{sku-code}")
     @ResponseStatus(HttpStatus.OK)
-    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
-        log.info("Received inventory check request for skuCode: {}", skuCode);
+    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
         return inventoryService.isInStock(skuCode);
     }
 }
--- inventory-service/src/main/java/com/springbootmicroservices/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/springbootmicroservices/inventoryservice/repository/InventoryRepository.java
@@ -3,8 +3,8 @@ package com.springbootmicroservices.inventoryservice.repository;
 import com.springbootmicroservices.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
