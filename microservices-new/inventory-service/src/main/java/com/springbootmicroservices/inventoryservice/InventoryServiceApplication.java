abdf32b5792e42cf87ce5675e06e3581364a0480
Chanuth10 <it20074418@my.sliit.lk>
2023-08-13 16:29:07 +0530

    adding discovery server and control load balancing

--- microservices-new/inventory-service/src/main/java/com/springbootmicroservices/inventoryservice/InventoryServiceApplication.java
+++ microservices-new/inventory-service/src/main/java/com/springbootmicroservices/inventoryservice/InventoryServiceApplication.java
@@ -2,9 +2,11 @@ package com.springbootmicroservices.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
