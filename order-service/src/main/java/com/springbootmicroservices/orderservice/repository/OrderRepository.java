8a421ffa55458fafa73fd6d8e642a2f11dcffd46
Chanuth <it20074418@my.sliit.lk>
2023-12-16 19:45:05 +0530

    implement intigration test for

--- order-service/src/main/java/com/springbootmicroservices/orderservice/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springbootmicroservices.orderservice.repository;
-
-import com.springbootmicroservices.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
--- product-service/src/main/java/com/springbootmicroservices/productservice/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springbootmicroservices.productservice.repository;
-
-import com.springbootmicroservices.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
