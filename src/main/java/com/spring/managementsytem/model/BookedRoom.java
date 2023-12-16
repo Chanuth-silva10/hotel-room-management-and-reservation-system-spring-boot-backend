7ce75a749eda1e1bbf7f011ba9d992ca51516d3b
Chanuth <it20074418@my.sliit.lk>
2023-12-17 00:46:59 +0530

    implement book and booking details model

--- src/main/java/com/spring/managementsytem/model/BookedRoom.java
+++ src/main/java/com/spring/managementsytem/model/BookedRoom.java
@@ -1,4 +1,64 @@
 package com.spring.managementsytem.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Entity // providing automatic generation of getters, setters, and constructors (with and without arguments).
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long bookingId;
+
+    @Column(name = "check_In")
+    private LocalDate checkInDate;
+    @Column(name = "check_Out")
+    private  LocalDate checkOutDate;
+    @Column(name = "Guest_FullName")
+    private String guestFullName;
+    @Column(name = "Guest_Email")
+    private String guestEmail;
+    @Column(name = "adults")
+    private int NumOfChildren;
+    @Column(name = "children")
+    private int NumOfAdults;
+    @Column(name = "total_guest")
+    private int totalNumOfGuest;
+    @Column(name = "Confirmation_Code")// specify that the field bookingConfirmationCode in a JPA entity should be mapped to a database column named "Confirmation_Code," defining the column name explicitly in the database schema.
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)//  association should be fetched lazily and uses the room_id column as the foreign key in the database.
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuest(){
+        this.totalNumOfGuest = this.NumOfAdults + NumOfChildren;
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        NumOfChildren = numOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        NumOfAdults = numOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+
+    public void setRoom(Room room) {
+    }
 }
--- src/main/java/com/spring/managementsytem/model/Room.java
+++ src/main/java/com/spring/managementsytem/model/Room.java
@@ -1,18 +1,44 @@
 package com.spring.managementsytem.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.math.BigDecimal;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity // providing automatic generation of getters, setters, and constructors
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
-
+    @Id // In this code snippet, @Id denotes that the field id is the primary key of a JPA entity, and @GeneratedValue(strategy = GenerationType.IDENTITY) specifies that the database will automatically generate unique values for this identifier using an identity column, typically used for auto-incrementing primary keys in relational databases.
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String roomType;
     private BigDecimal roomPrice;
     private boolean isBooked = false;
+    @Lob
+    private Blob photo;//typically represents binary large objects, such as images
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // if you save or delete the Room entity, the changes will be propagated to the associated BookedRoom entities.
     private List<BookedRoom> bookings;
 
     public Room() {
         this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom booking){
+        if (bookings == null){
+            bookings = new ArrayList<>();
+        }
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
     }
 }
