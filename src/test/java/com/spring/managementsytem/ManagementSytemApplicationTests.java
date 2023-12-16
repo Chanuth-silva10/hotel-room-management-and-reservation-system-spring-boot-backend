dea3a9e2e8908473a2869ede2539f0d4e192804d
Chanuth <it20074418@my.sliit.lk>
2023-12-16 22:22:27 +0530

    add new dependency

---
+++ src/test/java/com/spring/managementsytem/ManagementSytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.managementsytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
