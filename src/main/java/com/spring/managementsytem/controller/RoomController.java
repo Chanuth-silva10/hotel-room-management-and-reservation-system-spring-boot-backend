f8fdee1888db67b4d27701bfc1a97b43ffdfd92a
Chanuth <it20074418@my.sliit.lk>
2023-12-18 12:58:03 +0530

    implement get room type

--- src/main/java/com/spring/managementsytem/controller/RoomController.java
+++ src/main/java/com/spring/managementsytem/controller/RoomController.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,5 +33,10 @@ public class RoomController {
         RoomResponse response = new RoomResponse(savedRoom.getId(), savedRoom.getRoomType(),
                 savedRoom.getRoomPrice());
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/room/types")
+    public List<String> getRoomTypes() {
+        return roomService.getAllRoomTypes();
     }
 }
