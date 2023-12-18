c934e58c59c58c91fce82370ece729d701565158
Chanuth <it20074418@my.sliit.lk>
2023-12-19 02:33:21 +0530

    add exception  handling

---
+++ src/main/java/com/spring/managementsytem/exception/InternalServerException.java
@@ -0,0 +1,7 @@
+package com.spring.managementsytem.exception;
+
+public class InternalServerException extends RuntimeException {
+    public InternalServerException(String message) {
+        super(message);
+    }
+}
